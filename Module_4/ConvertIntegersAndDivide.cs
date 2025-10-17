4ad173c52a0cea8d02e1352044332b425716b375
aidan-flame01 <aidansjsingh@hotmail.com>
2025-10-17 14:58:45 -0400

    Update ConvertIntegersAndDivide.cs

--- Module_4/ConvertIntegersAndDivide.cs
+++ Module_4/ConvertIntegersAndDivide.cs
@@ -1,1 +1,24 @@
+using System;
 
+class Program2 
+{
+  static void Main (string[] args) 
+  {
+    Console.WriteLine("Enter the first number: ");
+    string input1 = Console.ReadLine();
+
+    Console.WriteLine("Enter the second number: ");
+    string input2 = Console.ReadLine();
+
+    int number1 =  Convert.ToInt32(input1);
+    int number2 = Convert.ToInt32(input2);
+
+    int result = Divide(number1, number2);
+    Console.WriteLine($"The result of dividing {number1} and {number2} is {result}");
+  }
+
+  static int Divide(int a, int b)
+  {
+    return a / b;
+  }
+}
